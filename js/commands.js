6f98ecdebff6ccc7fd72375b2fc4a9b367bb043d
Sree Vignesh <sreevignesh256@gmail.com>
2024-09-24 15:58:11 +0530

    Initial

---
+++ js/commands.js
@@ -0,0 +1,69 @@
+var password = "n0t4p@55w0rd!";
+var linkedin = "www.linkedin.com/in/sree-vignesh-c-807284249";
+var instagram = "https://www.instagram.com/w.i.kki?igsh=MWU4aHBzdmw5bXo1cg==";
+var github = "https://github.com/sree-vignesh/";
+var email = "mailto:reach.sreevignesh@gmail.com";
+
+whois = [
+  "<br>",
+  "Greetings, Im Sree Vignesh !",
+  "A web wizard with a fervent fascination for the realms of networking and operating systems.",
+  " I approach tech challenges like a daring adventurer exploring uncharted territories, ",
+  "crafting innovative solutions that transform the mundane into the extraordinary.",
+
+  "Armed with a diverse toolkit of languages and technologies, ",
+  "I immerse myself in personal quests that refine my skills and optimize my daily workflows.",
+  " If you seek a collaborator who merges technical prowess with a visionary spirit, ",
+  "let’s forge a connection and conjure up some extraordinary creations!",
+  "<br>",
+];
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>",
+];
+
+social = [
+  "<br>",
+  'linkedin       <a href="' +
+    linkedin +
+    '" target="_blank">linkedin/sree-vignesh' +
+    "</a>",
+  'instagram      <a href="' +
+    instagram +
+    '" target="_blank">instagram/sree-vignesh' +
+    "</a>",
+  'github         <a href="' +
+    github +
+    '" target="_blank">github/sree-vignesh' +
+    "</a>",
+  "<br>",
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>",
+];
+
+projects = [
+  "<br>",
+  "Will be updated here soon...meanwhile take a look at my GitHub and LinkedIn",
+  "<br>",
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          About myself',
+  // '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">social</span>         Display social networks',
+  // '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View my projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
